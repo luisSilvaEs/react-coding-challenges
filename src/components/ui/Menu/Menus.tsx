addf72632498eac2a1955cfa6a3c64092bf10472
Luis Silva <luissilva@Luiss-MacBook-Air.local>
2024-05-23 17:36:40 -0600

    Create menu mobile

--- src/components/ui/Menu/Menus.tsx
+++ src/components/ui/Menu/Menus.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import logoURL from "../../../assets/img/logo-app.png";
+import menuIMGURL from "../../../assets/img/mobile-menu-icon.png";
 import "./Menus.scss";
 
 const TopMenu = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    console.log("Clicked!");
+    setOpen((o) => !o);
+  };
+
   return (
-    <nav className="container mx-auto flex mt-2">
-      <div className="nav-logo-container">
-        <img src={logoURL} alt="" />
+    <nav className="container mx-auto mt-9 flex justify-around">
+      <div className={`nav-logo-container ${open && "hidden"}`}>
+        <img src={logoURL} alt="logo theater masks" />
       </div>
-      <div className="nav-links-container content-center mx-auto hidden md:block">
-        <ul className="flex">
+      <div
+        className={`nav-links-container content-center mx-auto hidden md:block ${
+          open && "open-mobile-menu"
+        }`}
+      >
+        <ul className="sm:flex-reverse md:flex">
           <li>
             <a href="">Home</a>
           </li>
@@ -20,6 +33,9 @@ const TopMenu = () => {
           </li>
         </ul>
       </div>
+      <button className={`md:hidden ${open && "mr-9"}`} onClick={toggleMenu}>
+        <img src={menuIMGURL} alt="menu mobile" />
+      </button>
     </nav>
   );
 };
