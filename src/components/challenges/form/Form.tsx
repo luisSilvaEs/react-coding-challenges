f60a99701aea320086e130045a21c21d3c76a7d1
Luis Silva <luissilva@Luiss-MacBook-Air.local>
2024-05-28 13:49:57 -0600

    Add validation for form email field

--- src/components/challenges/form/Form.tsx
+++ src/components/challenges/form/Form.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import "./Form.scss";
 
 const Form = () => {
-  const validateEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const [emailUser, setEmailUser] = useState<string>("");
+  const [error, setError] = useState<string>(""); //This notation is named "generic" and even no necessary but improves code readibily
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const handleSetEmailUser = (event: React.ChangeEvent<HTMLInputElement>) => {
     const email = event.target.value;
-    console.log("->", email);
+
+    if (emailRegex.test(email)) {
+      setError("");
+      setEmailUser(email);
+    }
+  };
+
+  const validateEmailOnLeaveInput = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const email = event.target.value;
+
+    if (!emailRegex.test(email)) {
+      setError("Please enter a valid email address.");
+    } else {
+      setError("");
+    }
   };
 
   return (
@@ -19,7 +41,43 @@ const Form = () => {
           <label htmlFor="email" className="requiered">
             Email:
           </label>
-          <input type="email" id="email" onChange={validateEmail} />
+          <input
+            className={
+              error
+                ? "error-email-message"
+                : emailRegex.test(emailUser)
+                ? "correct-email"
+                : ""
+            }
+            type="email"
+            id="email"
+            onBlur={validateEmailOnLeaveInput}
+            onChange={handleSetEmailUser}
+            required
+          />
+          {error && (
+            <div className="error-email-message">
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                role="img"
+                data-icon="CircleXSmall"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M14.5 8C14.5 11.5899 11.5899 14.5 8 14.5C4.41015 14.5 1.5 11.5899 1.5 8C1.5 4.41015 4.41015 1.5 8 1.5C11.5899 1.5 14.5 4.41015 14.5 8ZM16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM4.46967 5.53033L6.93934 8L4.46967 10.4697L5.53033 11.5303L8 9.06066L10.4697 11.5303L11.5303 10.4697L9.06066 8L11.5303 5.53033L10.4697 4.46967L8 6.93934L5.53033 4.46967L4.46967 5.53033Z"
+                  fill="currentColor"
+                ></path>
+              </svg>{" "}
+              <span>{error}</span>{" "}
+            </div>
+          )}
           <label htmlFor="password" className="requiered">
             Password:
           </label>
