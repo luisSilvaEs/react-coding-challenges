c6883a49573156d2ae1c4ab18fb82563c476846b
Luis Silva <luissilva@Luiss-MacBook-Air.local>
2024-05-31 23:07:11 -0600

    Update eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,31 @@
 module.exports = {
-    // Other configurations...
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:jsx-a11y/recommended',
+    ],
     rules: {
+      // Add your custom rules here
       '@typescript-eslint/no-redeclare': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
-      // Other rules...
     },
+    env: {
+      browser: true,
+      node: true,
+      es6: true,
+    },
+    plugins: ['@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y'],
   };
   
